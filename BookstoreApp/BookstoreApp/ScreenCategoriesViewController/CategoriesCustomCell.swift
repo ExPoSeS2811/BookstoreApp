d568ca8ad2d2a52b5bc207d9f1a09962ee99561b
Timofey Spodeneyko <timofeispd@gmail.com>
2023-12-05 22:52:37 +0300

    Add ScreenCategoriesViewController

---
+++ BookstoreApp/BookstoreApp/ScreenCategoriesViewController/CategoriesCustomCell.swift
@@ -0,0 +1,44 @@
+//
+//  CategoriesCustomCell.swift
+//  BookstoreApp
+//
+//  Created by Timofey Spodeneyko on 05.12.2023.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.textColor = .white
+        label.backgroundColor = .gray
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupCell()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupCell()
+    }
+
+    private func setupCell() {
+        addSubview(label)
+
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: topAnchor),
+            label.leadingAnchor.constraint(equalTo: leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor),
+            label.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+
+    func configure(with text: String) {
+        label.text = text
+    }
+}
